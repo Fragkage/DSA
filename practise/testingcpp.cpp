73afd908072bd52ae6856840c85e0d29a9a4cfcc
Fragkage <harshitrana1903@gmail.com>
2026-04-11 22:41:06 +0530

    Some practice

---
+++ practise/testingcpp.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter 2 numbers seperated by space to find their average"<< endl;
+    cin >> a >> b;
+    float c = (a+b)/2;
+    cout << "The average of the given numbers is " << c;
+
+    return 0;
+}
