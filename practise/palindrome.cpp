a7b800356a810d6fb0a9b71a528e3bdc54e8160a
Fragkage <harshitrana1903@gmail.com>
2026-04-20 20:55:13 +0530

    Palindrome question 1sst attempt

---
+++ practise/palindrome.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverseInt(int n) {
+    while (n>0) {
+        int reverse;
+        reverse = reverse*10 + n%10;
+        n = n/10;
+    }
+    return 
+}
+
+int checkPalindrome(int n){
+      
+}
+
+int main(){
+    int n ;
+    cout << "Enter a number to check if its a palindrome";
+    cin >>  n;
+}
