fee74ce66c9280a783d1c93353eff04a41f25ee9
Fragkage <harshitrana1903@gmail.com>
2026-04-20 21:45:11 +0530

    palindrome question completed

--- practise/palindrome.cpp
+++ practise/palindrome.cpp
@@ -2,20 +2,26 @@
 using namespace std;
 
 int reverseInt(int n) {
+    int reverse= 0;
     while (n>0) {
-        int reverse;
         reverse = reverse*10 + n%10;
         n = n/10;
     }
-    return 
+    return reverse;
 }
 
-int checkPalindrome(int n){
-      
+void checkPalindrome(int n){
+      if (reverseInt(n) == n) {
+        cout << "the number is palindrome";
+      }
+      else {
+        cout << "The number is not a palindrome";
+      }
 }
 
 int main(){
     int n ;
     cout << "Enter a number to check if its a palindrome";
     cin >>  n;
+    checkPalindrome(n);
 }
